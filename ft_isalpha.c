0a58cae222199c58190f8235c2343c0f558a4334
Сафар Сафарлы <emurky@am-b3.msk.21-school.ru>
2020-11-03 23:26:29 +0300

    added ft_isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,42 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emurky <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/02 21:05:30 by emurky            #+#    #+#             */
-/*   Updated: 2020/11/02 21:05:32 by emurky           ###   ########.fr       */
+/*   Created: 2020/11/03 23:15:56 by emurky            #+#    #+#             */
+/*   Updated: 2020/11/03 23:16:00 by emurky           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-//memset
-//bzero
-//memcpy
-//memccpy
-//memmove
-//memchr
-//memcmp
-unsigned long int	ft_strlen(const char *s);
-//strlcpy
-//strlcat
-//strchr
-//strrchr
-//strnstr
-//strncmp
-int					ft_atoi(const char *str);
-int					ft_isalpha(int c);
-int					ft_isdigit(int c);
-//isalnum
-//isascii
-//isprint
-//toupper
-//tolower
-//
-//calloc
-//strdup
-
-#endif
+int		ft_isalpha(int c)
+{
+	if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
