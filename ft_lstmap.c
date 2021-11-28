bb7ac2ae5111648442cf4021421e13afbc460c05
Safar Safarly <hafl@me.com>
2021-11-28 22:24:32 +0300

    ft_lstmap: del-pointer checking returned

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -17,7 +17,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_list;
 	t_list	*new_node;
 
-	if (!lst || !f)
+	if (!lst || !f || !del)
 		return (NULL);
 	new_list = NULL;
 	while (lst)
